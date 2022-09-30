00bce9ca45801bde230faa989188250c072e57b1
paras-j <59125176+paras-j@users.noreply.github.com>
2022-09-30 00:39:24 -0400

    Update 3_💬_Voices.py

--- pages/3_💬_Voices.py
+++ pages/3_💬_Voices.py
@@ -3,6 +3,33 @@ import streamlit as st
 st.title("Voice of the Stakeholder")
 
 
+
+# # config.py
+# TWITTER_CONSUMER_KEY = ''
+# TWITTER_CONSUMER_SECRET = ''
+# TWITTER_ACCESS_TOKEN = ''
+# TWITTER_ACCESS_TOKEN_SECRET = ''
+
+# TWITTER_USERNAMES = [
+#     'traderstewie',
+#     'the_chart_life',
+#     'canuck2usa',
+#     'sunrisetrader',
+#     'tmltrader'
+# ]
+
+# DB_HOST = '127.0.0.1'
+# DB_USER = 'postgres'
+# DB_PASS = 'password'
+# DB_NAME = 'etfdb'
+
+
+
+
+
+
+
+
 # import streamlit as st
 # import pandas as pd
 # import numpy as np
